cef81c0b7d2e9283a00be3d73e7f1123b19b3dbe
patelsaheb1219 <kaumik1219@gmail.com>
2021-07-20 20:43:51 -0400

    Created a showcase component for the homepage only

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,17 @@
 // Module Import
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 // Style Imports
 import styles from "@/styles/Layout.module.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import Showcase from "./Showcase";
 
 export default function Layout(props) {
   const { title, keywords, description, children } = props;
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -16,6 +20,7 @@ export default function Layout(props) {
         <meta name='keywords' content={keywords} />
       </Head>
       <Header />
+      {router.pathname === "/" && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+// File Import
+import styles from "@/styles/Showcase.module.css";
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to Party!</h1>
+      <h2>Find the hottest DJ Events</h2> 
+    </div>
+  )
+}
