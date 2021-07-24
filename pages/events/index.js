d79b8376a610bd768fc124042ddff90a21f99617
patelsaheb1219 <kaumik1219@gmail.com>
2021-07-23 20:03:20 -0400

    Designed eventItem, event and home page component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,29 @@
-// File Imports
+// Component Imports
 import Layout from "@/components/Layout";
 
-export default function Events() {
+// File/Variable Imports
+import { API_URL } from "@/config/index";
+import EventItem from "@/components/Event/EventItem";
+
+export default function EventsPage(props) {
+  const { events } = props;
   return (
     <Layout>
-      <h1>My events</h1>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
 // Component Imports
-import Layout from "@/components/Layout";
+import Link from "next/link";
 
 // File/Variable Imports
 import { API_URL } from "@/config/index";
+import EventItem from "@/components/Event/EventItem";
+import Layout from "@/components/Layout";
 
 export default function Home(props) {
   const { events } = props;
   return (
     <Layout>
-      {console.log(events)};
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View all Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -18,7 +30,7 @@ export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
   return {
-    props: {events},
-    revalidate: 1
-  }
+    props: { events: events.slice(0,3) },
+    revalidate: 1,
+  };
 }
